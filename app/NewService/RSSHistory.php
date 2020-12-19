2ba5f12eb0d5f3e1f6b4ed2dcf7359e771223b57
Wu Mobius <energy908@gmail.com>
2020-12-19 12:36:44 +0800

    modify RSSHistory service and repository

--- app/NewService/RSSHistory.php
+++ app/NewService/RSSHistory.php
@@ -18,18 +18,7 @@ class RSSHistory
 
     public function filterLatestRSS(Collection $RSSCollection): RSS
     {
-//        dump($RSSCollection);
-//        dump($RSSCollection->pluck('PostTime'));
-//
-//        $RSSCollection=collect([
-//            $RSSCollection[1],
-//            $RSSCollection[3],
-//            $RSSCollection[0],
-//            $RSSCollection[2],
-//                               ]);
-
         $LatestRSSTime = Carbon::createFromTimestamp(0);
-//        dump($LatestRSSTime);
         $RSSCollection->each(
             function ($rss) use (&$LatestRSSTime, &$LatestRSS) {
                 if ($rss->PostTime->greaterThan($LatestRSSTime)) {
@@ -38,20 +27,16 @@ class RSSHistory
                 }
             }
         );
-//        dump($LatestRSSTime);
-//        dump($LatestRSS);
         return $LatestRSS;
     }
 
-    public function update(string $subscribe_name,Carbon $current_post_time)
+    public function update(string $subscribe_name, Carbon $current_post_time)
     {
-        dump($subscribe_name,$current_post_time);
-        return $this->RSSHistoryRepo->updateCurrentTime($subscribe_name,$current_post_time);
+        return $this->RSSHistoryRepo->updateCurrentTime($subscribe_name, $current_post_time);
     }
 
     public function getLatest(string $subscribe_name)
     {
-//        dump(123);
         $LastPost = $this->RSSHistoryRepo->getLatestPost($subscribe_name);
         if (is_null($LastPost)) {
             $LastPost = $this->RSSHistoryRepo->create($subscribe_name);
--- app/Repository/RSSHistory.php
+++ app/Repository/RSSHistory.php
@@ -18,45 +18,23 @@ class RSSHistory
 
     public function getLatestPost($subscribe_name)
     {
-//        dump($subscribe_name);exit();
-//        dump($this->RSSHistoryModel->where('SubscribeName', 'like', '%'.$subscribe_name.'%')->first());
-//        exit();
+        $LastPost = $this->RSSHistoryModel->where('SubscribeName', 'like', $subscribe_name)->first();
 
-//        return $this->RSSHistoryModel->firstOrCreate(
-//            [
-//                'SubscribeName' => $subscribe_name,
-//                'CurrentPostTime' => Carbon::now()->toDateTimeString()
-//            ]
-//        );
-$LastPost=$this->RSSHistoryModel->where('SubscribeName', 'like', '%'.$subscribe_name.'%')->first();
-//if(is_null($LastPost)){
-//    $LastPost=new RSSHistoryModel();
-//    $LastPost->SubscribeName= $subscribe_name;
-//    $LastPost->CurrentPostTime= Carbon::now()->toDateTimeString();
-//    $LastPost->save();
-//
-//}
         return $LastPost;
-
-//        return $this->RSSHistoryModel->where('SubscribeName', 'like', '%'.$subscribe_name.'%')->firstOrCreate(
-//            [
-//                'SubscribeName' => $subscribe_name,
-//                'CurrentPostTime' => Carbon::now()->toDateTimeString()
-//            ]
-//        );
     }
 
     public function updateCurrentTime(string $subscribe_name, Carbon $LastCurrentTime)
     {
-        $LastPost=$this->RSSHistoryModel->where('SubscribeName', 'like', '%'.$subscribe_name.'%')->first();
-        $LastPost->CurrentPostTime=$LastCurrentTime->toDateTimeString();
+        $LastPost = $this->RSSHistoryModel->where('SubscribeName', 'like', '%' . $subscribe_name . '%')->first();
+        $LastPost->CurrentPostTime = $LastCurrentTime->toDateTimeString();
         return $LastPost->save();
     }
 
-    public function create(string $subscribe_name){
-        $LastPost=new RSSHistoryModel();
-        $LastPost->SubscribeName= $subscribe_name;
-        $LastPost->CurrentPostTime= Carbon::now()->toDateTimeString();
+    public function create(string $subscribe_name)
+    {
+        $LastPost = new RSSHistoryModel();
+        $LastPost->SubscribeName = $subscribe_name;
+        $LastPost->CurrentPostTime = Carbon::now()->toDateTimeString();
         $LastPost->save();
         return $LastPost;
     }
