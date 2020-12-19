ae6ef4ae1766662b80d2486b7d89b094d2efe81f
Wu Mobius <energy908@gmail.com>
2020-12-19 12:39:58 +0800

    rearrange MessengerNotification code

--- app/Notifications/MessengerNotification.php
+++ app/Notifications/MessengerNotification.php
@@ -5,8 +5,6 @@ namespace App\Notifications;
 use App\NewService\Message\Discord\DiscordMessageFactory;
 use Awssat\Notifications\Messages\DiscordMessage;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Str;
 
@@ -14,7 +12,8 @@ class MessengerNotification extends Notification
 {
     use Queueable;
 
-private $Observer;
+    private $Observer;
+
     /**
      * Create a new notification instance.
      *
@@ -22,46 +21,49 @@ private $Observer;
      */
     public function __construct(string $observer)
     {
-        $this->Observer=$observer;
+        $this->Observer = $observer;
     }
 
     /**
      * Get the notification's delivery channels.
      *
-     * @param  mixed  $notifiable
+     * @param mixed $notifiable
      * @return array
      */
     public function via($notifiable)
     {
 //        dump($notifiable->getMessengerType($this->Observer));
-        $target=Str::lower($notifiable->getMessengerType($this->Observer));
+        $target = Str::lower($notifiable->getMessengerType($this->Observer));
 //dump($target);
-        return ['discord'];
+        return [$target];
     }
 
-public function toDiscord($notifiable){
-        $DiscordMessageFactory=new DiscordMessageFactory();
-        $DiscordMessage= $DiscordMessageFactory->createMessage($notifiable);
+    public function toDiscord($notifiable)
+    {
+        $DiscordMessageFactory = new DiscordMessageFactory();
+        $DiscordMessage = $DiscordMessageFactory->createMessage($notifiable);
         return $DiscordMessage->exportDiscordMessage();
-    return (new DiscordMessage)
-            ->content('Taylor Otwell ⛵️ / @taylorotwell 有一則新的貼文')
-            ->embed(
-                function ($embed) {
-                    $embed->title(
-                        'Twitter',
-                        'https://laravel.com/docs/8.x/notifications#slack-notifications'
-                    )
-                        ->color('#1FA1F1')
-                        ->description('post content')
-                        ->footer('2020.12.20')
-                        ->author(
-                            'Taylor Otwell ⛵️ / @taylorotwell',
-                            'https://laravel.com/docs/8.x/notifications#slack-notifications',
-                            'https://i.imgur.com/LS08Auh.png'
-                        )
-                    ->thumbnail( 'https://nitter.net/pic/profile_images%2F914894066072113152%2FpWD-GUwG_400x400.jpg');
-                }
-            );
-}
+//        return (new DiscordMessage)
+//            ->content('Taylor Otwell ⛵️ / @taylorotwell 有一則新的貼文')
+//            ->embed(
+//                function ($embed) {
+//                    $embed->title(
+//                        'Twitter',
+//                        'https://laravel.com/docs/8.x/notifications#slack-notifications'
+//                    )
+//                        ->color('#1FA1F1')
+//                        ->description('post content')
+//                        ->footer('2020.12.20')
+//                        ->author(
+//                            'Taylor Otwell ⛵️ / @taylorotwell',
+//                            'https://laravel.com/docs/8.x/notifications#slack-notifications',
+//                            'https://i.imgur.com/LS08Auh.png'
+//                        )
+//                        ->thumbnail(
+//                            'https://nitter.net/pic/profile_images%2F914894066072113152%2FpWD-GUwG_400x400.jpg'
+//                        );
+//                }
+//            );
+    }
 }
 
