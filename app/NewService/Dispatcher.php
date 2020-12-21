c4fbb32b7bce3edede457566ffb5ecae5c55e2d7
Wu Mobius <energy908@gmail.com>
2020-12-21 11:15:09 +0800

    modify Dispatcher.php

--- app/NewService/Dispatcher.php
+++ app/NewService/Dispatcher.php
@@ -13,26 +13,19 @@ class Dispatcher
 
     public function broadcastAll(Collection $RSSCollection)
     {
-//        $RSSItem->notify();
         $RSSCollection->each(
             function ($RSS) {
                 $this->broadcast($RSS);
-//            exit();
             }
         );
     }
 
     public function broadcast(RSS $RSSItem)
     {
-//        dump($RSSItem);
-//        $all_wait_to_notify=$RSSItem->Subscribe->observer;
         $Observers = $RSSItem->getObservers();
-//        dump($Observers);
-//        $all_wait_to_notify->each(function ($item){
-//            $RSSItem->notify();
-        // Job::dispatch
-        //(one message , many target)
-//        });
+        dump('Job::dispatch');
+        dump($Observers);
+
         $Observers->each(
             function ($observer) use ($RSSItem) {
                 NotificationJob::dispatch($RSSItem, $observer)->onQueue('Notification');
