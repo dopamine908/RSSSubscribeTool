e09c8eff99b071cc5084349a76f40bf493482440
Wu Mobius <energy908@gmail.com>
2020-12-21 11:39:24 +0800

    add rss:go command to schedule, every five minute frequency

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,7 +3,6 @@
 namespace App\Console;
 
 use App\Console\Commands\CrawlRSSCommand;
-use App\Service\Dispatcher;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -21,16 +20,12 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      *
-     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @param \Illuminate\Console\Scheduling\Schedule $schedule
      * @return void
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
-//        $schedule->call(function (){
-//            $dispatcher = new Dispatcher(new \App\Service\RSSItemFactory());
-//            $dispatcher->execute();
-//        })->everyThirtyMinutes();
+        $schedule->command(CrawlRSSCommand::class)->everyTenMinutes();
     }
 
     /**
@@ -40,7 +35,7 @@ class Kernel extends ConsoleKernel
      */
     protected function commands()
     {
-        $this->load(__DIR__.'/Commands');
+        $this->load(__DIR__ . '/Commands');
 
         require base_path('routes/console.php');
     }
